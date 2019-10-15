0d0b4e0f943ec95d92c6666a9a8e532ca7be911c
Karl Hepworth <karl.hepworth@gmail.com>
2019-10-15 23:49:53 +1100

    Add port 443 to haproxy
    Signed-off-by: Karl Hepworth <karl.hepworth@gmail.com>

--- service/haproxy/haproxy.go
+++ service/haproxy/haproxy.go
@@ -16,6 +16,8 @@ func New() model.Service {
 			"-d",
 			"-p",
 			"80:80",
+			"-p",
+			"443:443",
 			"--volume=/var/run/docker.sock:/tmp/docker.sock",
 			"--restart=always",
 			"--name=amazeeio-haproxy",
