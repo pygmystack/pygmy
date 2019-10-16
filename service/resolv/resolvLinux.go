027c6dd79bde09604ad13771895923f7142eebd8
Karl Hepworth <karl.hepworth@gmail.com>
2019-10-16 22:35:50 +1100

    Fix the linux resolver - woo.
    Signed-off-by: Karl Hepworth <karl.hepworth@gmail.com>

--- service/resolv/resolvLinux.go
+++ service/resolv/resolvLinux.go
@@ -24,21 +24,50 @@ func run(args []string) error {
 
 func (resolv Resolv) Configure() {
 
-	fullPath := fmt.Sprintf("%v%v%v", resolv.Path, string(os.PathSeparator), resolv.File)
-	if _, err := os.Stat(fullPath); os.IsNotExist(err) {
-		// Create the directory if it doesn't exist.
-		if _, err := os.Stat(resolv.Path); os.IsNotExist(err) {
-			err := run([]string{"sudo", "mkdir", "-p", resolv.Path})
-			if err != nil {
-				fmt.Println(err)
+	if resolv.Status() {
+		model.Green(fmt.Sprintln("Already configured resolvr"))
+	} else {
+		fullPath := fmt.Sprintf("%v%v%v", resolv.Path, string(os.PathSeparator), resolv.File)
+		if _, err := os.Stat(fullPath); os.IsNotExist(err) {
+			// Create the directory if it doesn't exist.
+			if _, err := os.Stat(resolv.Path); os.IsNotExist(err) {
+				err := run([]string{"sudo", "mkdir", "-p", resolv.Path})
+				if err != nil {
+					fmt.Println(err)
+				}
+				err = run([]string{"sudo", "chmod", "777", resolv.Path})
+				if err != nil {
+					fmt.Println(err)
+				}
 			}
-			err = run([]string{"sudo", "chmod", "777", resolv.Path})
-			if err != nil {
-				fmt.Println(err)
+			// Create the file if it doesn't exist.
+			if _, err := os.Stat(fullPath); os.IsNotExist(err) {
+				tmpFile, error := ioutil.TempFile("", "pygmy-")
+				if error != nil {
+					fmt.Println(error)
+				}
+				error = os.Chmod(tmpFile.Name(), 0777)
+				if error != nil {
+					fmt.Println(error)
+				}
+				_, error = tmpFile.WriteString(resolv.Contents)
+				if error != nil {
+					fmt.Println(error)
+				}
+				err := run([]string{"sudo", "cp", tmpFile.Name(), fullPath})
+				if err != nil {
+					fmt.Println(err)
+				}
 			}
 		}
-		// Create the file if it doesn't exist.
-		if _, err := os.Stat(fullPath); os.IsNotExist(err) {
+		if _, err := os.Stat(fullPath); err == nil {
+
+			cmd := exec.Command("/bin/sh", "-c", "cat "+fullPath)
+			cmdOut, cmdErr := cmd.Output()
+			if cmdErr != nil {
+				model.Red(cmdErr.Error())
+			}
+
 			tmpFile, error := ioutil.TempFile("", "pygmy-")
 			if error != nil {
 				fmt.Println(error)
@@ -47,6 +76,7 @@ func (resolv Resolv) Configure() {
 			if error != nil {
 				fmt.Println(error)
 			}
+			_, error = tmpFile.WriteString(string(cmdOut))
 			_, error = tmpFile.WriteString(resolv.Contents)
 			if error != nil {
 				fmt.Println(error)
@@ -56,38 +86,12 @@ func (resolv Resolv) Configure() {
 				fmt.Println(err)
 			}
 		}
-	}
-	if _, err := os.Stat(fullPath); err == nil {
 
-		cmd := exec.Command("/bin/sh", "-c", "cat "+fullPath)
-		cmdOut, cmdErr := cmd.Output()
-		if cmdErr != nil {
-			model.Red(cmdErr.Error())
+		if resolv.Status() {
+			model.Green(fmt.Sprintf("Successfully configured local resolver"))
+		} else {
+			model.Red(fmt.Sprintf("Could not configure local resolver"))
 		}
-
-		tmpFile, error := ioutil.TempFile("", "pygmy-")
-		if error != nil {
-			fmt.Println(error)
-		}
-		error = os.Chmod(tmpFile.Name(), 0777)
-		if error != nil {
-			fmt.Println(error)
-		}
-		_, error = tmpFile.WriteString(string(cmdOut))
-		_, error = tmpFile.WriteString(resolv.Contents)
-		if error != nil {
-			fmt.Println(error)
-		}
-		err := run([]string{"sudo", "cp", tmpFile.Name(), fullPath})
-		if err != nil {
-			fmt.Println(err)
-		}
-	}
-
-	if resolv.Status() {
-		model.Green(fmt.Sprintf("Successfully configured local resolver"))
-	} else {
-		model.Red(fmt.Sprintf("Could not configur local resolver"))
 	}
 
 }
