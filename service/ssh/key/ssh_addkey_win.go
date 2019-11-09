95790797dbb6a66f78405fe258f4b5d4915726c7
Karl Hepworth <karl.hepworth@gmail.com>
2019-11-09 20:43:10 +1100

    Update windows ssh key package for windows friendliness
    Signed-off-by: Karl Hepworth <karl.hepworth@gmail.com>

--- service/ssh/key/ssh_addkey_win.go
+++ service/ssh/key/ssh_addkey_win.go
@@ -4,33 +4,30 @@ package key
 
 import (
 	"github.com/docker/docker/api/types/container"
-	"github.com/docker/docker/api/types/mount"
 	"github.com/docker/docker/api/types/network"
 	model "github.com/fubarhouse/pygmy/v1/service/interface"
 )
 
 func NewAdder(key string) model.Service {
 	return model.Service{
-		Name: "amazeeio-ssh-agent-add-key",
+		Name:     "amazeeio-ssh-agent-add-key",
 		Discrete: true,
-		Output: true,
-		Config:        container.Config{
-			Image:    "amazeeio/ssh-agent",
+		Output:   true,
+		Config: container.Config{
+			Image: "amazeeio/ssh-agent",
 			Cmd: []string{
 				"windows-key-add",
 				"/key",
 			},
-		},
-		HostConfig:    container.HostConfig{
-			AutoRemove:  true,
-			Mounts: []mount.Mount{
-				{
-					Type: "bind",
-					Source: key,
-					Target: key,
-					ReadOnly: true,
-				},
+			Labels: map[string]string{
+				"pygmy": "pygmy",
 			},
+		},
+		HostConfig: container.HostConfig{
+			IpcMode:     "private",
+			AutoRemove:  true,"
+			Binds:       []string{fmt.Sprintf("%v:/%v", key, key)},
+			VolumesFrom: []string{"amazeeio-ssh-agent"},
 		},
 		NetworkConfig: network.NetworkingConfig{},
 	}
@@ -38,15 +35,20 @@ func NewAdder(key string) model.Service {
 
 func NewShower() model.Service {
 	return model.Service{
-		ContainerName: "amazeeio-ssh-agent-show-keys",
-		Config:        container.Config{
-			Image:    "amazeeio/ssh-agent",
+		Name: "amazeeio-ssh-agent-show-keys",
+		Discrete: true,
+		Output: true,
+		Config: container.Config{
+			Image: "amazeeio/ssh-agent",
 			Cmd: []string{
 				"ssh-add",
 				"-l",
 			},
+			Labels:		map[string]string{
+				"pygmy": "pygmy",
+			},
 		},
-		HostConfig:    container.HostConfig{
+		HostConfig: container.HostConfig{
 			AutoRemove:  true,
 			VolumesFrom: []string{"amazeeio-ssh-agent"},
 		},
