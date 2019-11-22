6e3bb9a9417f806280c53d21d94b1688c6e44ec6
Karl Hepworth <karl.hepworth@gmail.com>
2019-11-22 21:25:00 +1100

    Implements fix for #21
    Signed-off-by: Karl Hepworth <karl.hepworth@gmail.com>

--- service/haproxy/haproxy.go
+++ service/haproxy/haproxy.go
@@ -3,12 +3,13 @@ package haproxy
 import (
 	"github.com/docker/docker/api/types/container"
 	"github.com/docker/docker/api/types/network"
+	"github.com/docker/go-connections/nat"
 	model "github.com/fubarhouse/pygmy-go/service/interface"
 )
 
 func New() model.Service {
 	return model.Service{
-		Name: "amazeeio-haproxy",
+		Name:   "amazeeio-haproxy",
 		Weight: 14,
 		Config: container.Config{
 			Image: "amazeeio/haproxy",
@@ -17,13 +18,29 @@ func New() model.Service {
 			},
 		},
 		HostConfig: container.HostConfig{
-			Binds:      []string{"/var/run/docker.sock:/tmp/docker.sock"},
-			AutoRemove: false,
+			Binds:        []string{"/var/run/docker.sock:/tmp/docker.sock"},
+			AutoRemove:   false,
+			PortBindings: nil,
 			RestartPolicy: struct {
 				Name              string
 				MaximumRetryCount int
 			}{Name: "always", MaximumRetryCount: 0},
 		},
 		NetworkConfig: network.NetworkingConfig{},
+	}
+}
+
+func NewDefaultPorts() model.Service {
+	return model.Service{
+		HostConfig: container.HostConfig{
+			PortBindings: nat.PortMap{
+				"80/tcp": []nat.PortBinding{
+					{
+						HostIP:   "",
+						HostPort: "80",
+					},
+				},
+			},
+		},
 	}
 }
