5d4446406abb6b903fe64945581260ca28309689
Karl Hepworth <karl.hepworth@gmail.com>
2020-01-06 22:46:04 +1100

    Potential logic fix for unhandled tcp listen error
    Signed-off-by: Karl Hepworth <karl.hepworth@gmail.com>

--- service/library/dryrun.go
+++ service/library/dryrun.go
@@ -36,12 +36,21 @@ func DryRun(c *Config) []CompatibilityCheck {
 							} else {
 								conn, err := net.Listen("tcp", ":"+p)
 								if conn != nil {
-									defer conn.Close()
+									if e := conn.Close(); e != nil {
+										fmt.Println(e)
+									}
 								}
-								messages = append(messages, CompatibilityCheck{
-									State:   false,
-									Message: fmt.Sprintf("[ ] %v is not able to start on port %v: %v", Service.Name, p, err),
-								})
+								if err != nil {
+									messages = append(messages, CompatibilityCheck{
+										State:   false,
+										Message: fmt.Sprintf("[ ] %v is not able to start on port %v: %v", Service.Name, p, err),
+									})
+								} else {
+									messages = append(messages, CompatibilityCheck{
+										State:   true,
+										Message: fmt.Sprintf("[*] %v is able to start on port %v", Service.Name, p),
+									})
+								}
 							}
 						}
 					}
