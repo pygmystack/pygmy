8a1025425690ac860665164b1ad26db721b4bca2
Karl Hepworth <karl.hepworth@gmail.com>
2020-02-22 07:47:11 +1100

    Add missing labels for default behaviour
    Signed-off-by: Karl Hepworth <karl.hepworth@gmail.com>

--- service/ssh/key/ssh_addkey.go
+++ service/ssh/key/ssh_addkey.go
@@ -42,6 +42,7 @@ func NewShower() model.Service {
 				"-L",
 			},
 			Labels: map[string]string{
+				"pygmy.defaults": "true",
 				"pygmy.enable":   "true",
 				"pygmy.name":     "amazeeio-ssh-agent-show-keys",
 				"pygmy.discrete": "true",
