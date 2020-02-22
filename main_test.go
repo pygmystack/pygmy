f3feafcf3e42e1a32e919d3cffdda04b97023e5c
Karl Hepworth <karl.hepworth@gmail.com>
2020-02-22 22:01:06 +1100

    #183: Update complex test
    Signed-off-by: Karl Hepworth <karl.hepworth@gmail.com>

--- main_test.go
+++ main_test.go
@@ -209,7 +209,7 @@ func TestCustom(t *testing.T) {
 		configpath:         "/examples/pygmy.complex.yml",
 		endpoints:          []string{"http://traefik.docker.amazee.io", "http://mailhog.docker.amazee.io", "http://portainer.docker.amazee.io", "http://phpmyadmin.docker.amazee.io"},
 		images:             []string{"amazeeio/ssh-agent", "mailhog/mailhog", "phpmyadmin/phpmyadmin", "portainer/portainer", "library/traefik:v2.1.3"},
-		services:           []string{"unofficial-portainer", "unofficial-traefik-2", "unofficial-phpmyadmin", "amazeeio-ssh-agent", "amazeeio-mailhog"},
+		services:           []string{"unofficial-portainer", "unofficial-traefik-2", "unofficial-phpmyadmin", "amazeeio-mailhog"},
 		servicewithports:   []string{"amazeeio-mailhog", "unofficial-portainer", "unofficial-phpmyadmin", "unofficial-traefik-2"},
 		skipendpointchecks: false,
 	}
