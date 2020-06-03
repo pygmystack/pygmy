4359613585f6bfae94140847fcc7771c64059c92
Karl Hepworth <karl.hepworth@gmail.com>
2020-06-03 22:53:53 +1000

    Add git change detection
    Signed-off-by: Karl Hepworth <karl.hepworth@gmail.com>

--- service/library/version.go
+++ service/library/version.go
@@ -11,12 +11,14 @@ import (
 var (
 	// Version tag used on local builds in Travis.
 	VERSIONTAG = os.Getenv("TRAVIS_COMMIT")
-)
 
-const (
 	// Fixed version which is modified via travis for the
 	// release builds. Should match version with v prepended.
 	RELEASETAG = ""
+
+	// Custom indicator will be added if changes are detected
+	// to pygmy, so it would read dev-xxxxxxx-custom
+	CUSTOMTAG = ""
 )
 
 // Version describes which version of Pygmy is running. This will be kept
@@ -26,7 +28,7 @@ const (
 func Version(c Config) {
 
 	// RELEASETAG will be provided via `sed` in the build pipeline.
-	if RELEASETAG != "" {
+	if RELEASETAG != "" && len(RELEASETAG) >= 7 {
 		if match, _ := regexp.Match("^v[0-9]+.[0-9]+.[0-9]+$", []byte(RELEASETAG)); match {
 			fmt.Printf("Pygmy %v\n", RELEASETAG)
 			return
@@ -39,22 +41,26 @@ func Version(c Config) {
 	// Get tags and reference information.
 	tags, _ := exec.Command("git", "show-ref", "--tags").Output()
 	ref, _ := exec.Command("git", "rev-parse", "HEAD").Output()
+	_, changes := exec.Command("git", "diff-index", "--quiet", "HEAD").Output()
+	if changes != nil {
+		CUSTOMTAG = "-custom"
+	}
 
 	// Scan the references from the tags to check if the current reference is
 	// associated to a tag. This is to show the tag version on a local build
 	// from source.
 	for _, tag := range strings.Split(string(tags), "\n") {
 		if strings.Contains(tag, string(ref)) {
-			fmt.Printf("Pygmy %v", strings.SplitAfter(tag, "/")[2])
+			fmt.Printf("Pygmy %v%v\n", strings.SplitAfter(tag, "/")[2], CUSTOMTAG)
 			return
 		}
 	}
 
 	if VERSIONTAG != "" {
 		// If the version tag isn't empty:
-		fmt.Printf("Pygmy %v", VERSIONTAG)
+		fmt.Printf("Pygmy %v%v", VERSIONTAG, CUSTOMTAG)
 	} else {
 		// If we don't have a version tag, use a reference.
-		fmt.Printf("Pygmy version dev-%v\n", string(ref[0:7]))
+		fmt.Printf("Pygmy version dev-%v%v\n", string(ref[0:7]), CUSTOMTAG)
 	}
 }
