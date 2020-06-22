fa2e0a0db6a52dd1f6edb6a944a1550e9e80c7be
Karl Hepworth <karl.hepworth@gmail.com>
2020-06-22 21:49:45 +1000

    #226: Fix automated versioning (#228)
    * improved local actions support
    * Move logic which causes ordering issue
    * Streip references before processing
    * Update comments
    * Replace go mod install hook for a command
    * Add the commit sha and echo the values
    * Rewrite the version logic
    * Add a small section on releasing via github actions.
    * Make use of act clearer - local only.
    * Remove needless committag parse
    * remove needless loop value
    * Add a test case for running github actions in travis
    * add go install to github action sim in travis
    * use root.💣
    * travis github actions test package name change
    * move go install location; change path to binaries.
    * add go mod vendor to github action sim
    * travis update
    * Remove travis test for github action - will do it later
    * add sudo to goreleaser

--- service/library/version.go
+++ service/library/version.go
@@ -2,17 +2,22 @@ package library
 
 import (
 	"fmt"
-	"os"
 	"os/exec"
 	"regexp"
 	"strings"
 )
 
 var (
-	// Version tag used on local builds in Actions.
-	COMMITSHA = os.Getenv("GITHUB_SHA")
 
-	// Fixed version which is modified via travis for the
+	// isPygmy will determine if the local git branch is Pygmy.
+	// otherwise the mechanisms to allow non-pygmy hash refs
+	// being shown.
+	isPygmy = false
+
+	// Version tag used on local builds in Actions.
+	COMMITSHA = ""
+
+	// Fixed version which is modified via github actions for the
 	// release builds. Should match version with v prepended.
 	COMMITTAG = ""
 
@@ -27,51 +32,45 @@ var (
 // after the release is published.
 func Version(c Config) {
 
+	// The Commit SHA is provided to GitHub Actions, if it is not set when compiled
+	// we assume git is being used. Let's grab metadata from the repository for use
+	// in the runtime -
 	if COMMITSHA == "" {
-		COMMITTAG = os.Getenv("TRAVIS_COMMIT")
+		r, _ := exec.Command("git", "remote", "-v").Output()
+		remotes := strings.Split(string(r), "\n")
+		for remote := range remotes {
+			if strings.Contains(remotes[remote], "pygmy-go") {
+				isPygmy = true
+			}
+		}
+
+		sha, _ := exec.Command("git", "rev-parse", "HEAD").Output()
+		COMMITSHA = string(sha)
+
+		_, changes := exec.Command("git", "diff-index", "--quiet", "HEAD").Output()
+		if changes != nil {
+			CUSTOMTAG = "-custom"
+		}
 	}
 
-	// RELEASETAG and VERSIONTAG should be set when running Actions.
-	// Travis will add a commit has which should not print in this section.
-	if COMMITTAG != "" {
-		reference := ""
-		if strings.Contains(COMMITTAG, "/") {
-			reference = strings.Split(COMMITTAG, "/")[2]
-		} else {
-			reference = COMMITTAG
-		}
+	// Detect a tagged version:
+	if !isPygmy && COMMITTAG != "" {
+		b := strings.Split(COMMITTAG, "/")
+		reference := b[len(b)-1]
+
 		if match, _ := regexp.Match("^v[0-9]+.[0-9]+.[0-9]+$", []byte(reference)); match {
-			fmt.Printf("Pygmy %v\n", reference)
-			return
-		} else if match, _ := regexp.Match("^[0-9|a-z|A-Z]+$", []byte(reference)); match {
-			fmt.Printf("Pygmy version dev-%v\n", reference[0:7])
+			fmt.Printf("Pygmy %v%v\n", reference, CUSTOMTAG)
 			return
 		}
 	}
 
-	// Get tags and reference information.
-	tags, _ := exec.Command("git", "show-ref", "--tags").Output()
-	ref, _ := exec.Command("git", "rev-parse", "HEAD").Output()
-	_, changes := exec.Command("git", "diff-index", "--quiet", "HEAD").Output()
-	if changes != nil {
-		CUSTOMTAG = "-custom"
-	}
-
-	// Scan the references from the tags to check if the current reference is
-	// associated to a tag. This is to show the tag version on a local build
-	// from source.
-	for _, tag := range strings.Split(string(tags), "\n") {
-		if strings.Contains(tag, string(ref)) {
-			fmt.Printf("Pygmy %v%v\n", strings.SplitAfter(tag, "/")[2], CUSTOMTAG)
+	// Detect a SHA reference:
+	if isPygmy || COMMITSHA != "" {
+		if match, _ := regexp.Match("^[0-9|a-z|A-Z]+$", []byte(COMMITSHA)); match {
+			fmt.Printf("Pygmy version dev-%v%v\n", COMMITSHA[0:7], CUSTOMTAG)
 			return
 		}
-	}
-
-	if COMMITTAG != "" {
-		// If the version tag isn't empty:
-		fmt.Printf("Pygmy %v%v\n", COMMITTAG, CUSTOMTAG)
 	} else {
-		// If we don't have a version tag, use a reference.
-		fmt.Printf("Pygmy version dev-%v%v\n", string(ref[0:7]), CUSTOMTAG)
+		fmt.Printf("Pygmy version unidentifiable.\n")
 	}
 }
