420d04bb4164b5ff7545384fa2125d8bac5f80a1
Karl Hepworth <karl.hepworth@gmail.com>
2020-07-16 08:52:58 +1000

    gofmt
    Signed-off-by: Karl Hepworth <karl.hepworth@gmail.com>

--- service/dnsmasq/dnsmasq_test.go
+++ service/dnsmasq/dnsmasq_test.go
@@ -26,7 +26,7 @@ func Test(t *testing.T) {
 		So(obj.HostConfig.AutoRemove, ShouldBeFalse)
 		So(fmt.Sprint(obj.HostConfig.CapAdd), ShouldEqual, fmt.Sprint([]string{"NET_ADMIN"}))
 		So(obj.HostConfig.IpcMode, ShouldEqual, "private")
-		So(fmt.Sprint(obj.HostConfig.PortBindings), ShouldEqual, fmt.Sprint(nat.PortMap{"53/tcp": []nat.PortBinding{{HostIP:   "", HostPort: "6053",},}, "53/udp": []nat.PortBinding{{HostIP:   "", HostPort: "6053"}}}))
+		So(fmt.Sprint(obj.HostConfig.PortBindings), ShouldEqual, fmt.Sprint(nat.PortMap{"53/tcp": []nat.PortBinding{{HostIP: "", HostPort: "6053"}}, "53/udp": []nat.PortBinding{{HostIP: "", HostPort: "6053"}}}))
 		So(obj.HostConfig.RestartPolicy.Name, ShouldEqual, "on-failure")
 		So(obj.HostConfig.RestartPolicy.MaximumRetryCount, ShouldBeZeroValue)
 	})
--- service/network/network_test.go
+++ service/network/network_test.go
@@ -19,7 +19,7 @@ func Test(t *testing.T) {
 		So(obj.Name, ShouldEqual, "amazeeio-network")
 		So(obj.IPAM.Driver, ShouldEqual, "")
 		So(obj.IPAM.Options, ShouldEqual, nil)
-		So(fmt.Sprint(obj.IPAM.Config), ShouldEqual, fmt.Sprint([]network.IPAMConfig{{Subnet:  "10.99.99.0/24", Gateway: "10.99.99.1"}}))
+		So(fmt.Sprint(obj.IPAM.Config), ShouldEqual, fmt.Sprint([]network.IPAMConfig{{Subnet: "10.99.99.0/24", Gateway: "10.99.99.1"}}))
 		So(fmt.Sprint(obj.Labels), ShouldEqual, fmt.Sprint(map[string]string{"pygmy.name": "amazeeio-network"}))
 	})
 }
