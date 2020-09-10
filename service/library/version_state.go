c36aebd57ef04e8968bdb139eabb92243434bf9f
Karl Hepworth <karl.hepworth@gmail.com>
2020-09-10 21:13:20 +1000

    Replace the version printing mechanism
    Signed-off-by: Karl Hepworth <karl.hepworth@gmail.com>

---
+++ service/library/version_state.go
@@ -0,0 +1,13 @@
+package library
+
+import "fmt"
+
+var PYGMY_VERSION = ""
+
+func printversion() bool {
+	if PYGMY_VERSION == "" {
+	  return false
+	}
+	fmt.Printf("Pygmy version v%v\n", PYGMY_VERSION)
+	return true
+}
