7a0787eb0a6fc9f1b4b1e90bd9663d9d1effc3b6
Karl Hepworth <karl.hepworth@gmail.com>
2021-11-22 23:38:01 +1100

    Proposed solution for #348
    Signed-off-by: Karl Hepworth <karl.hepworth@gmail.com>

--- service/library/version_state.go
+++ service/library/version_state.go
@@ -1,15 +1,23 @@
 package library
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 // PYGMY_VERSION is the equivalent to the version pygmy is being associated to.
 // This variable is exclusively used when packaging a formal release.
 var PYGMY_VERSION = ""
 
 func printversion() bool {
+	parts := strings.Split(PYGMY_VERSION, "/")
 	if PYGMY_VERSION == "" {
 		return false
 	}
-	fmt.Printf("Pygmy version v%v\n", PYGMY_VERSION)
+	resultVersion :=  fmt.Sprintf("%v", parts[len(parts)-1])
+	if !strings.HasPrefix(resultVersion, "v") {
+		resultVersion = fmt.Sprintf("v%v", resultVersion)
+	}
+	fmt.Printf("Pygmy version %v", resultVersion)
 	return true
 }
