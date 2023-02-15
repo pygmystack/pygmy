21350aca7ef293d8fa890317d0bafd5678e189e4
Karl Hepworth <karl.hepworth@gmail.com>
2023-02-15 21:26:17 +1100

    Adjust docker container termination pointer reference
    Signed-off-by: Karl Hepworth <karl.hepworth@gmail.com>

--- service/interface/docker/docker.go
+++ service/interface/docker/docker.go
@@ -173,9 +173,9 @@ func DockerStop(name string) error {
 	if err != nil {
 		return err
 	}
-	var timeout *int
-	*timeout = 10
-	err = cli.ContainerStop(ctx, name, container.StopOptions{Timeout: timeout})
+	var timeout int
+	timeout = 10
+	err = cli.ContainerStop(ctx, name, container.StopOptions{Timeout: &timeout})
 	if err != nil {
 		return err
 	}
