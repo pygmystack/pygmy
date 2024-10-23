e784e9e00c5f4a3d46629ee716ee6f4d70db310e
Karl <karl.hepworth@gmail.com>
2024-10-23 13:17:00 +1100

    update interface
    Signed-off-by: Karl <karl.hepworth@gmail.com>

--- internal/runtime/runtime.go
+++ internal/runtime/runtime.go
@@ -1,21 +1,27 @@
 package runtime
 
+import (
+	"context"
+
+	"github.com/docker/docker/client"
+)
+
 // ServiceRuntime is the definition of a Container Runtime for compatability with Pygmy.
 type ServiceRuntime interface {
-	Setup() error
-	Start() error
-	Create() error
-	Status() (bool, error)
-	Labels() (map[string]string, error)
+	Setup(ctx context.Context, cli *client.Client) error
+	Start(ctx context.Context, cli *client.Client) error
+	Create(ctx context.Context, cli *client.Client) error
+	Status(ctx context.Context, cli *client.Client) (bool, error)
+	Labels(ctx context.Context, cli *client.Client) (map[string]string, error)
 	// @TODO: Does ID() work better as retrieving digests?
-	ID() (string, error)
-	Clean() error
-	Stop() error
-	StopAndRemove() error
-	Remove() error
+	ID(ctx context.Context, cli *client.Client) (string, error)
+	Clean(ctx context.Context, cli *client.Client) error
+	Stop(ctx context.Context, cli *client.Client) error
+	StopAndRemove(ctx context.Context, cli *client.Client) error
+	Remove(ctx context.Context, cli *client.Client) error
 
-	SetField(name string, value interface{}) error
-	GetFieldString(field string) (string, error)
-	GetFieldInt(field string) (int, error)
-	GetFieldBool(field string) (bool, error)
+	SetField(ctx context.Context, cli *client.Client, name string, value interface{}) error
+	GetFieldString(ctx context.Context, cli *client.Client, field string) (string, error)
+	GetFieldInt(ctx context.Context, cli *client.Client, field string) (int, error)
+	GetFieldBool(ctx context.Context, cli *client.Client, field string) (bool, error)
 }
